07d20e001b1a03a8affb3263dc4cdd46db76fb70
Ankit Rajpoot <rajputankit22@gmail.com>
2025-02-11 00:00:38 +0530

    first commit

--- pattern/pattern-15.cpp
+++ pattern/pattern-15.cpp
@@ -1,3 +1,10 @@
+// A B C D E 
+// A B C D 
+// A B C 
+// A B 
+// A 
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,9 +12,8 @@ void print1(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        char pr = 'A' + i;
-        for(int j=0; j <= i ; j++){
-            cout << pr << " ";
+        for(char j='A'; j < 'A'+n-i ; j++){
+            cout << j << " ";
         }
         cout << endl;
     }
--- pattern/pattern-16.cpp
+++ pattern/pattern-16.cpp
@@ -1,3 +1,10 @@
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
