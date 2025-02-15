213434030d3e32c76d5a57ca53e507993748b4bd
Ankit Rajpoot <rajputankit22@gmail.com>
2025-02-15 16:21:54 +0530

    add multi set

--- demo.cpp
+++ demo.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool comp(pair<int, int> p1, pair<int, int> p2){
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+
+    if(p1.first > p2.first) return true;
+    return false;
+}
+
 void explainMap(int n)
 {
-  
-    map<int, int> m;
-    m.insert({1,2});
-    m.emplace(3,4);
 
-    auto len = m.size();
-    cout << len << endl;
+    // pair<int, int> a[] = {{1,2},{2,1},{4,1}};
+    // sort(a, a+3, comp);
 
-    for(auto it : m){
-        cout << it.first << " " << it.second << endl;
-    }
+    // for(auto it : a){
+    //     cout << it.first << " " << it.second << endl;
+    // }
+
+    // int num = 6;
+    // int count = __builtin_popcount(num);
+    // cout << count << endl;
+
+    string s = "231";
+    sort(s.begin(), s.end());
+    do {
+        cout << s << endl;
+    }while(next_permutation(s.begin(), s.end()));
+
+    
+
 }
 int main()
 {
